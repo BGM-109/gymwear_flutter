4682bf95cc5695b41147ff5c89cfe3a4ccc07b5b
BGM-109 <vividxxxxx@gmail.com>
2022-05-17 16:07:43 +0900

    feat: 링크 기능 추가

--- lib/view/widgets/card.dart
+++ lib/view/widgets/card.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:gymwear_app/model/gymwear_model.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class CustomCard extends StatelessWidget {
   final GymwearModel gm;
@@ -11,33 +12,40 @@ class CustomCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      margin: const EdgeInsets.only(bottom: 16.0),
-      padding: const EdgeInsets.all(16.0),
-      decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(8.0),
-          border: Border.all(color: Colors.black, width: 2.0)),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          CircleAvatar(
-            backgroundImage: NetworkImage(gm.logo.url),
-          ),
-          const SizedBox(
-            height: 8.0,
-          ),
-          Text(
-            gm.name,
-            style: const TextStyle(fontWeight: FontWeight.bold),
-          ),
-          const SizedBox(
-            height: 8.0,
-          ),
-          Text(
-            gm.shopLink,
-            style: const TextStyle(fontSize: 12.0),
-          ),
-        ],
+    return InkWell(
+      onTap: () {
+        launchUrl(
+          Uri.parse(gm.shopLink),
+        );
+      },
+      child: Container(
+        margin: const EdgeInsets.only(bottom: 16.0),
+        padding: const EdgeInsets.all(16.0),
+        decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(8.0),
+            border: Border.all(color: Colors.black, width: 2.0)),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            CircleAvatar(
+              backgroundImage: NetworkImage(gm.logo.url),
+            ),
+            const SizedBox(
+              height: 8.0,
+            ),
+            Text(
+              gm.name,
+              style: const TextStyle(fontWeight: FontWeight.bold),
+            ),
+            const SizedBox(
+              height: 8.0,
+            ),
+            Text(
+              gm.shopLink,
+              style: const TextStyle(fontSize: 12.0),
+            ),
+          ],
+        ),
       ),
     );
   }
